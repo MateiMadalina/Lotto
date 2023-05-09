a4e90079a4e16d3bef6fa4004845007101e1e2cd
appuser <appuser@journey2-54b966f9c6-mxqmh>
2023-05-09 15:53:40 +0000

    starter project

---
+++ src/main/java/com/codecool/lotto/Application.java
@@ -0,0 +1,17 @@
+package com.codecool.lotto;
+
+public class Application {
+    public static void main(String[] args){
+        int quantity = 5;
+        int maxNumber = 10;
+    }
+
+    //Game Logic
+    //
+
+    //User Input
+    //
+
+    //Output
+    //
+}
